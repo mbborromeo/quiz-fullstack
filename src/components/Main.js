836844723f81fea05110c2ab86293dc8cc904d4d
Michael Borromeo <michael.borromeo@digitalistgroup.com>
2023-04-29 17:30:17 -0700

    only allow users access if they initially provided a username

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,20 @@
 import React, { useRef } from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { setUserId } from "../redux/result_reducer";
 
 import "../styles/Main.css";
 
 export default function Main() {
   const inputRef = useRef(null);
+  const dispatch = useDispatch();
+
+  function startQuiz() {
+    // only allow user to take quiz if inputted email address
+    if (inputRef.current?.value) {
+      dispatch(setUserId(inputRef.current?.value));
+    }
+  }
 
   return (
     <div className="container">
@@ -28,7 +38,7 @@ export default function Main() {
       </form>
 
       <div className="start">
-        <Link className="btn" to={"quiz"}>
+        <Link className="btn" to={"quiz"} onClick={startQuiz}>
           Start Quiz
         </Link>
       </div>
